38031ca5ef8234e244d230e8001d6355a82e5155
Alex Rodríguez <alejandro.rodriguez@upf.edu>
2023-03-09 12:53:03 +0100

    fix mipmaps

--- src/vkCore.cpp
+++ src/vkCore.cpp
@@ -1475,7 +1475,6 @@ void VkCore::createTextureImage()
 
     VkBuffer stagingBuffer;
     VkDeviceMemory stagingBufferMemory;
-
     createBuffer(imageSize, VK_BUFFER_USAGE_TRANSFER_SRC_BIT, 
         VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT, stagingBuffer, stagingBufferMemory);
 
@@ -1592,7 +1591,7 @@ void VkCore::transitionImageLayout(VkImage image, VkFormat format, VkImageLayout
 
 void VkCore::createTextureImageView()
 {
-    textureImageView = createImageView(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_ASPECT_COLOR_BIT, 1);
+    textureImageView = createImageView(textureImage, VK_FORMAT_R8G8B8A8_SRGB, VK_IMAGE_ASPECT_COLOR_BIT, mipLevels);
 }
 
 
@@ -1616,7 +1615,7 @@ void VkCore::createTextureSampler()
     samplerInfo.compareEnable = VK_FALSE;
     samplerInfo.compareOp = VK_COMPARE_OP_ALWAYS;
     samplerInfo.mipmapMode = VK_SAMPLER_MIPMAP_MODE_LINEAR;
-    samplerInfo.minLod = 5.0f;
+    samplerInfo.minLod = 0.0f;
     samplerInfo.maxLod = static_cast<float>(mipLevels);
     samplerInfo.mipLodBias = 0.0f;
 
